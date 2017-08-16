46b5ae924beedeb2c8a6b2faa8f3587beda10860
Siebren Zwerver <siebren.zwerver@gmail.com>
2017-08-16 15:39:13 +0200

    No tests on spec/*

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -28,4 +28,5 @@ require 'hiera-puppet-helper'
 RSpec.configure do |c|
     c.formatter = 'documentation'
     c.mock_with :rspec
+    c.exclude_pattern = /pkg\/**\/*|vendor\/**\/*|spec\/**\/*/,
 end
